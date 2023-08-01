dad95cd5f3dbafeae23b571744dbe6ed6aa69f5d
rajabca04 <rajabca201804@gmail.com>
2023-08-01 13:39:16 +0530

    change

---
+++ main.js
@@ -0,0 +1,31 @@
+// const heading = React.createElement("h1",
+//  {
+//     className: "main-heading",
+//     id: "main-heading",
+// }, "Hello World from React!");
+
+
+
+const parent = React.createElement("div",{id:"parent"},
+[
+    React.createElement("div",{id:"child1"},
+[
+    React.createElement("h1",{}," h1 Hello i am raja calling form child"),
+    React.createElement("h2",{}," h2 Hello i am raja calling form child")
+
+]),
+React.createElement("div",{id:"child2"},
+[
+    React.createElement("h1",{}," h1 Hello i am raja calling form child"),
+    React.createElement("h2",{}," h2 Hello i am raja calling form child")
+
+])
+])
+
+
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
