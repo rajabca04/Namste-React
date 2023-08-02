316e95df4295514c3b03edd7ed179c8e50cbbf5c
rajabca04 <rajabca201804@gmail.com>
2023-08-02 11:41:30 +0530

    change

--- main.js
+++ main.js
@@ -4,25 +4,20 @@
 //     id: "main-heading",
 // }, "Hello World from React!");
 
+import React from "react";
 
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div",{id:"parent"},
-[
-    React.createElement("div",{id:"child1"},
-[
-    React.createElement("h1",{}," h1 Hello i am raja calling form child"),
-    React.createElement("h2",{}," h2 Hello i am raja calling form child")
-
-]),
-React.createElement("div",{id:"child2"},
-[
-    React.createElement("h1",{}," h1 Hello i am raja calling form child"),
-    React.createElement("h2",{}," h2 Hello i am raja calling form child")
-
-])
-])
-
-
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, " h1 Hello i am raja calling form child"),
+    React.createElement("h2", {}, " h2 Namste react child"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, " h1 Hello i am raja calling form child"),
+    React.createElement("h2", {}, " h2 Hello i am raja calling form child"),
+  ]),
+]);
 
 console.log(parent);
 
